dc46e12b96658190dceb58a30d13c9a564d848b4
Leonardus Pedro Hidayat <leonardus.hidayat@cp.co.id>
2024-06-17 14:17:06 +0700

    Save Dashboard

--- components/NavBottom.vue
+++ components/NavBottom.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-flow-col gap-1 justify-between px-6 fixed bottom-6 w-full">
+    <div class="bg-bgBlue shadow-inner rounded-t-3xl grid grid-flow-col gap-1 flex justify-evenly px-6 py-6 h-15 content-center fixed bottom-0 w-full">
         <NuxtLink to="" class="">
             <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <mask id="mask0_2037_122" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="25" height="25">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}'
+  ],
   theme: {
     extend: {
       colors: {
