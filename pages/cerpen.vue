4854d8bae2426e752692122d1995dea13dbeddbc
ernifang <ernifang0563@gmail.com>
2024-07-06 05:29:16 +0700

    menambahkan beberapa seperti learning dan
    menambahkan beberapa page seperti learning dan

---
+++ pages/cerpen.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container mx-auto px-4 py-20 flex items-center justify-between relative">
+        <NuxtLink to="/flow/section-header" class="flex-grow text-center">
+            <h1 class="font-bold text-cyan-800 text-3xl">Apa itu Cerita Pendek ?</h1>
+        </NuxtLink>
+        <button class="bg-black rounded-full w-12 h-12 flex items-center justify-center text-amber-400 ml-auto">
+            <svg class="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a7.003 7.003 0 00-5-6.712V4a2 2 0 10-4 0v.288A7.003 7.003 0 004 11v3.159c0 .53-.214 1.039-.595 1.436L2 17h5m7 0a3 3 0 01-6 0m6 0H9"></path>
+            </svg>
+            <!-- <span class="absolute top-0 right-0 inline-flex items-center justify-center px-1 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">3</span> -->
+        </button>
+    </div>
+    <NavBottom />
+</template>
---
+++ pages/learning.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="learning-container mx-auto px-4 py-10 grid content-center">
+        <div class="flex justify-between place-content-evenly">
+            <div class="w-1/1">
+                    <img src="/public/img/learning.png" class="w-[200px] h-[200px]">
+            </div>
+            <div class="w-1/2">
+        <h2 class="card-title font-bold text-cyan-800 text-2xl content-center place-items-center">CERITA PENDEK</h2>
+            </div> 
+        </div>
+
+        <div class="mt-5">
+            <div class="Active">
+                <h2 class="font-semibold text-cyan-800 text-2xl text-left">Kegiatan 1</h2>
+            <button @click="routeToApp('Apa itu Cerita Pendek?', 'Pengertian Cerita Pendek')" type="button" class="text-cyan-800 block w-full text-left font-semibold p-4 bg-white rounded-xl text-xl mt-4">Apa itu cerita Pendek?  </button>
+            </div>
+
+            <div class="Active">
+                <h6 class="font-semibold text-cyan-800 text-2xl text-left">Kegiatan 2</h6>
+                <button @click="routeToApp('Unsur Pembangun Cerpen', 'Unsur Pembangun Cerpen')" type="button" class="text-cyan-800 block w-full text-left font-semibold p-4 bg-white rounded-xl text-xl mt-4">Unsur Pembangun Cerpen</button>
+            </div>
+                
+            <div class="Active">
+                <h6 class="font-semibold text-cyan-800 text-2xl text-left">Kegiatan 3</h6>
+                <button @click="routeToApp('Nilai-Nilai Kehidupan', 'Nilai-Nilai Kehidupan')" type="button" class="text-cyan-800 block w-full text-left font-semibold p-4 bg-white rounded-xl text-xl mt-4">Nilai-Nilai Kehidupan</button>
+            </div>
+
+            <div class="Active">
+                <h6 class="font-semibold text-cyan-800 text-2xl text-left">Kegiatan 4</h6>
+                <button @click="routeToApp('Menulis Cerpen', 'Menulis Cerpen')" type="button" class="text-cyan-800 block w-full text-left font-semibold p-4 bg-white rounded-xl text-xl mt-4">Menulis Cerpen</button>
+            </div>
+        </div>
+    </div>
+<NavBottom />
+</template>
+
+<script>
+export default {
+  name: 'Learning',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    selectActivity(activity) {
+      eventBus.emit('activity-selected', activity);
+    },
+    routeToApp(activity,description) {
+      this.$router.push({
+        path: '/app',
+        query: {
+          activity: activity,
+          description: description
+
+        }
+      })
+    }
+  }
+};
+</script>
+
+<style>
+/* Your styles here */
+.learning-container {
+  display: grid;
+}
+  /* Add any necessary styles for the container */
+   .card-title {
+  justify-content: center;
+  align-items: center;
+  width: 10px;
+  margin-bottom: 100px;
+  font-size: 40px ;
+
+}
+
+.Active {
+  margin-bottom: 40px;
+}
+</style>
