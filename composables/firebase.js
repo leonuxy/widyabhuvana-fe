9ee91dccd079716c04970a4c7ad03c25d3ad5b80
Michael Lesmana <michaellesmana39@gmail.com>
2024-07-16 22:29:34 +0700

    - Integrasi Project ke Firebase - Integrasi p. signup.vue ke firebase done - Integrasi p. login.vue ke firebase on-progress

---
+++ composables/firebase.js
@@ -0,0 +1,26 @@
+// Import the Firebase SDK
+import {initializeApp} from '@firebase/app';
+import {createUserWithEmailAndPassword, getAuth} from '@firebase/auth';
+
+// Initialize Firebase with your project's configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB5ZpeG87jFAvVvVz1hNbw8YU7IgyY7Tyg",
+    authDomain: "widyabhuvana.firebaseapp.com",
+    projectId: "widyabhuvana",
+    storageBucket: "widyabhuvana.appspot.com",
+    messagingSenderId: "1067203953420",
+    appId: "1:1067203953420:web:e09b76f5b6a012fb607f29",
+    measurementId: "G-5WPMHBZQGX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Get the Firebase Authentication object
+const auth = getAuth(app);
+
+// Export the auth object for use in other components
+export const useFirebase = () => ({
+    auth,
+    createUserWithEmailAndPassword, // Export the function
+});
--- pages/login.vue
+++ pages/login.vue
@@ -1,41 +1,57 @@
 <template>
-    <div class="container mx-auto p-4">
-        <NuxtLink to="/">
-            <img src="/img/svg/arrow_left_alt.svg" alt="">
-        </NuxtLink>
-        <div class="grid content-center">
-            <img src="/img/logo.png" alt="" class="w-28 mx-auto mb-10 mt-6">
-            <h3 class="font-bold text-3xl">Masuk</h3>
-            <p class="text-sm italic">Masuk untuk mulai belajar di sini.</p>
-    
-            <div class="grid gap-6 mt-4">
-                <FormsInput
-                    label="Surat Elektronik"
-                    type="email"
-                    placeholder="Tuliskan Surat Elektronik"
-                    id="surat-elektronik"
-                />
+  <div class="container mx-auto p-4">
+    <NuxtLink to="/">
+      <img alt="" src="/img/svg/arrow_left_alt.svg">
+    </NuxtLink>
+    <div class="grid content-center">
+      <img alt="" class="w-28 mx-auto mb-10 mt-6" src="/img/logo.png">
+      <h3 class="font-bold text-3xl">Masuk</h3>
+      <p class="text-sm italic">Masuk untuk mulai belajar di sini.</p>
 
-                <FormsInput
-                    label="Kata Sandi"
-                    type="password"
-                    placeholder="Tuliskan Kata Sandi"
-                    id="kata-sandi"
-                />
-            </div>
-            <a href="" class="block w-full text-right text-sc m pr-2 mt-2 text-secondaryBlue">Lupa kata sandi</a>
-            <a href="/dashboard" class="text-white block w-full text-center font-semibold p-4 bg-primaryBlue rounded-xl text-xl mt-4">Masuk</a>
-        </div>
+      <div class="grid gap-6 mt-4">
+        <FormsInput
+            id="surat-elektronik"
+            v-model="email"
+            label="Surat Elektronik"
+            placeholder="Tuliskan Surat Elektronik"
+            type="email"
+        />
+
+        <FormsInput
+            id="kata-sandi"
+            v-model="password"
+            label="Kata Sandi"
+            placeholder="Tuliskan Kata Sandi"
+            type="password"
+        />
+      </div>
+      <a class="block w-full text-right text-sc m pr-2 mt-2 text-secondaryBlue" href="">Lupa kata sandi</a>
+      <button class="text-white block w-full text-center font-semibold p-4 bg-primaryBlue rounded-xl text-xl mt-4"
+              @click="login">Masuk
+      </button>
     </div>
+  </div>
 </template>
 
 <script setup>
-const user = ref({
-  username: '',
-  password: '',
-});
+import {ref} from 'vue';
+import {useRouter} from 'vue-router';
+import {useFirebase} from '../composables/firebase';
+
+const router = useRouter();
+const {auth} = useFirebase(); // Asumsikan Anda memiliki composable untuk Firebase
+
+const email = ref('');
+const password = ref('');
+const error = ref(null);
 
 const login = async () => {
-  // TODO send user Data to the login endpoint and redirect if  successful 
+  try {
+    await auth.signInWithEmailAndPassword(email.value, password.value);
+    router.push('/dashboard'); // Redirect ke dashboard setelah login
+  } catch (error) {
+    console.error('Login error:', error);
+    error.value = error.message;
+  }
 };
 </script>
